d77773cf7b30dbd731a68ab7881cefaa81072a10
Ludwik1526 <mmichal1526@gmail.com>
2019-03-16 22:06:13 +0100

    Simple map changer

---
+++ AR2/Assets/Scripts/MapSelection.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapSelection : MonoBehaviour
+{
+    public GameObject display;
+    private MeshRenderer meshRenderer;
+    public Button bOrto;
+    public Button bHipso;
+    public Button bRegio;
+    public Button bKomm;
+    public Material orto;
+    public Material hipso;
+    public Material regio;
+    public Material komm;
+
+    void Start ()
+    {
+        meshRenderer = display.GetComponent<MeshRenderer>();
+        meshRenderer.material = orto;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        bOrto.onClick.AddListener(setOrto);
+        bHipso.onClick.AddListener(setHipso);
+        bRegio.onClick.AddListener(setRegio);
+        bKomm.onClick.AddListener(setKomm);
+    }
+
+    void setOrto()
+    {
+        meshRenderer.material = orto;
+    }
+
+    void setHipso()
+    {
+        meshRenderer.material = hipso;
+    }
+
+    void setRegio()
+    {
+        meshRenderer.material = regio;
+    }
+
+    void setKomm()
+    {
+        meshRenderer.material = komm;
+    }
+}
