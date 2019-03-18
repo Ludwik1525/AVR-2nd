95a208934881679cd78aaff04f26595c20ea12d7
Ludwik1526 <mmichal1526@gmail.com>
2019-03-18 15:39:42 +0100

    Progress? I guess?

---
+++ AR2/Assets/MapPlacement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GoogleARCore;
+using UnityEngine;
+
+public class MapPlacement : MonoBehaviour
+{
+
+
+    public GameObject display;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        Touch touch;
+        if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+        TrackableHit hit;
+        TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon |
+                                          TrackableHitFlags.FeaturePointWithSurfaceNormal;
+        if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
+        {
+            if (hit.Trackable is DetectedPlane)
+            {
+
+                display.transform.position = hit.Pose.position;
+                display.transform.rotation = hit.Pose.rotation;
+
+                var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+
+                display.transform.parent = anchor.transform;
+            }
+        }
+    }
+}
